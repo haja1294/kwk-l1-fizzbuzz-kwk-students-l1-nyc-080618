9f16d27fc9676ee4b0748caffe8e80ec93f0660e
Haja Trawally <hajatrawally6@gmail.com>
2018-08-08 13:12:43 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 def fizzbuzz (num)
 if num % 3 == 0 && num % 5 !== 0
 puts"fizz"
-  elsif num % 5 == 0 && num % 3 !==0
+  elsif num % 5 == 0 && num % 3 !== 0
   puts "buzz"
   elsif num % 3 == 0 && num % 5 == 0
   puts "fizzbuzz"
