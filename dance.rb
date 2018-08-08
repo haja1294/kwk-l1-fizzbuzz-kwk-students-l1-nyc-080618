74fcbf836f74431631d08b72c953f0588b261ce7
Haja Trawally <hajatrawally6@gmail.com>
2018-08-08 14:12:43 +0000

    automatically backed up by learn

--- dance.rb
+++
@@ -1,9 +0,0 @@
-# write a while loop
-# output "step to the left?""step to right" 5 times 
-
-counter = 0
-while counter < 5 
-puts "step to the left "
-puts "step to the right"
-counter=counter + 1 
-end
