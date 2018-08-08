4c3b7325fe9ef94d5d90a34c134e7c52fbf1b9d1
Haja Trawally <hajatrawally6@gmail.com>
2018-08-08 14:25:42 +0000

    automatically backed up by learn

--- T_swift.rb
+++ T_swift.rb
@@ -4,15 +4,16 @@ play=0
  until play == 5
   puts"Play"
   play += 1
-
+end 
 
 
 shake_it_out
 
 puts "and the hater's gonna"
-hate=0 
-until hate== 5 
+hate = 0 
+until hate == 5 
 puts "Hate"
-play +=1 
+play += 1 
 end 
+end
 shake_it_out
