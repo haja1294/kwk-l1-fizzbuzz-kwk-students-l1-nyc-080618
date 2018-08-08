399504dac7b4bb8054ac118250b2883868af4e2f
Haja Trawally <hajatrawally6@gmail.com>
2018-08-08 13:11:44 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,7 +3,7 @@ if num % 3 == 0 && num % 5 !== 0
 puts"fizz"
   elsif num % 5 == 0 && num % 3 !==0
   puts "buzz"
-  elsif num % 3 == 0 && num % 5 !== 0 
+  elsif num % 3 == 0 && num % 5 == 0
   puts "fizzbuzz"
   end 
 end
